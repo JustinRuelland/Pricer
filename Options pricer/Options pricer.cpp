ee6c327e4113fa708fce2e8d158b3d39a5f2253d
Louis Geist <louisjjgeist@gmail.com>
2023-01-13 23:52:42 +0100

    .gitignore taken into account

--- Options pricer/Options pricer.cpp
+++ Options pricer/Options pricer.cpp
@@ -1,29 +1,12 @@
-// Options pricer.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
-#include <random>
+#include "cdf.h"
 
 
 int main()
 {
-	std::default_random_engine generator;
-	std::normal_distribution<double> distribution(0.0,1.0);
-
-	double n = distribution(generator); // Attention, là la seed de l'aléatoire est toujours la même
-	double n2 = distribution(generator);
-
-	std::cout << "Yes, nombre aléatoire d'une centrée réduite"<<n << "et : "<< n2 << std::endl;
 	std::cout << "Hello World!\n";
+	int n = 100000;
+	double d = 0;
+
+	std::cout<<cdf(d,n)<<std::endl;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
