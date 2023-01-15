b8a31b74b0c3868cac07c3cf7455e9472fcaaf92
Louis Geist <louisjjgeist@gmail.com>
2023-01-15 22:16:06 +0100

    get and set functions for interface option

--- Options pricer/option.h
+++ Options pricer/option.h
@@ -10,5 +10,14 @@ protected :
 
 public :
 	virtual double price() const = 0;
-	
+
+	double get_K() {return this->K;};
+	double get_S() { return this->S; };
+	double get_T() { return this->T; };
+	double get_sigma() { return this->sigma; };
+
+	void set_K(double& K) { this->K = K; };
+	void set_S(double& S) { this->S = S; };
+	void set_T(double& T) { this->T = T; };
+	void set_sigma(double& sigma) { this->sigma = sigma; };
 };
