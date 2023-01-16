7fe44d79553113aac504ff6bf68545b529f286c9
Justin RUELLAND <justinruelland@gmail.com>
2023-01-16 15:34:01 +0100

    Class for assets v1

---
+++ Options pricer/asset.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class assets {
+	
+	protected :
+	double SpotPrice = NULL;
+	double volatility = NULL;
+
+	int DividensType = NULL; 
+	/*  An asset without dividends =0; 
+		An asset with lump payment of dividends =1; 
+		An asset with continuous dividend payment =2; */
+}
