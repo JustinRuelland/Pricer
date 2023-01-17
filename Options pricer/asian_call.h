6631228bef7b6a92e9cf293c0b75d52875b45760
Louis Geist <louisjjgeist@gmail.com>
2023-01-17 18:17:18 +0100

    Asian_call v1

---
+++ Options pricer/asian_call.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "option.h"
+
+class asian_call : public option {
+public:
+	// Constructors
+	asian_call() {};
+	asian_call(double K, double S, double T, double sigma) :option(K, S, T, sigma) {};
+	asian_call(asian_call& call) :option(call) {};
+
+	// Destructor
+	~asian_call();
+
+	// Member function
+	double price() const;
+};
