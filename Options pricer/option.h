02b9174a199cfaa97b12e1501136197e1db34c65
Louis GEIST <louisjjgeist@gmail.com>
2023-01-29 14:12:00 +0100

    option.h ; delete of the initialization with empty_asset ptr ; final version

--- Options pricer/option.h
+++ Options pricer/option.h
@@ -8,8 +8,7 @@ class option {
 protected : 
 	double K = 0 ; // strike
 	double T = 0; // maturity (in years)
-	asset empty_asset;
-	asset* ptr_underlying = &empty_asset;
+	asset* ptr_underlying = nullptr;
 
 public :
 	// Constructor
