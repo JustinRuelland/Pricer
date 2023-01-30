b0861eb00550afb3bb2dbe3d7ce2982fa442b0df
Louis GEIST <louisjjgeist@gmail.com>
2023-01-30 15:49:26 +0100

    main.cpp ; test of nullptr bug and test gap ; alpha

--- Options pricer/main.cpp
+++ Options pricer/main.cpp
@@ -5,15 +5,17 @@
 #include "asian_call.h"
 #include "asian_put.h"
 #include "asset.h"
+#include "gap_call.h"
+#include "gap_put.h"
 
 int main()
 {
 	std::cout << "Hello World!fef\n";
 
-	double K = 50.0;
-	double S = 40.0;
-	double T = 2;
-	double sigma = 0.30 ;
+	double K = 60.0;
+	double S = 60.0;
+	double T = 1;
+	double sigma = 0.1 ;
 	//european_call call1(K, S, T, sigma);
 
 	char name[20];
@@ -30,14 +32,14 @@ int main()
 	Asset1.set_Volatility(sigma);
 	Asset1.set_CurrentTime(0.0);
 
-	Asset1.get_alias_Dividends().set_Type(1);
+	Asset1.get_alias_Dividends().set_Type(0);
 	Asset1.get_alias_Dividends().set_Rate(0.0125);
 	Asset1.get_alias_Dividends().set_Periods(0.25);
 	Asset1.get_alias_Dividends().set_Next(0.1666);
 
 	asset* ptr_asset1 =&Asset1;
 	std::cout << Asset1 << std::endl;
-	european_call mon_call(ptr_asset1, K, T);
+	european_call mon_call;
 
 	
 
@@ -49,6 +51,19 @@ int main()
 
 	asian_put mon_asiat(ptr_asset1, K, T);
 	cout << mon_asiat.price() << endl;
+
+
+	cout << mon_call << endl;
+
+
+	// Illustrer la gestion d'un affichage d'une option créée par défaut
+	european_put default_put;
+	cout << default_put << endl;
+
+	// Test gap
+	gap_call mon_gap(ptr_asset1,K, T, K);
+	cout << mon_gap.price() << endl;
+
 	
 	return 0;
 }
