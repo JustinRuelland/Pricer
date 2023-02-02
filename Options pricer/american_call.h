734c02a4d5ef1829dcad950fd1babdce7d37c6c4
Justin RUELLAND <justinruelland@icloud.com>
2023-02-02 14:55:37 +0100

    American call h; creation and adding

---
+++ Options pricer/american_call.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "tools.h"
+#include "option.h"
+
+class american_call : public option {
+public :
+	// Constructors
+	american_call() {};
+	american_call(american_call& call) :option(call) {};
+	american_call(asset* ptr_underlying, double K, double T) : option(ptr_underlying, K, T) {};
+
+	// Destructor
+	~american_call() ;
+
+	// Member function
+	double price() const;
+	//string type() const;
+};
